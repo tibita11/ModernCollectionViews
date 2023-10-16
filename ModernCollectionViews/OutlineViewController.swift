c6ec6791b1864890e95a8199a7f33e944e136eda
tibita11 <tibita0811@gmail.com>
2023-10-16 23:50:32 +0900

    initial commit

---
+++ ModernCollectionViews/OutlineViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModernCollectionViews
+//
+//  Created by 鈴木楓香 on 2023/10/16.
+//
+
+import UIKit
+
+class OutlineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
